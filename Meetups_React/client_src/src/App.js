22d31c485ae741e482b64a6ab16b11627fbb8af5
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-05-26 02:57:39 -0400

    react setups

---
+++ Meetups_React/client_src/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+
+//just used to display main component and navbar component
+const App = () => (
+  <div>
+    <h1>My App </h1>
+    </div>
+
+)
+
+export default App;
